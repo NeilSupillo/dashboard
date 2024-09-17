d4042b7cf6d1b6de649beb56afa87a0eff192f52
MrCLDSA <167674440+MrCLDSA@users.noreply.github.com>
2024-09-17 11:27:11 +0800

    Updated composer lock and vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 
+                    'resources/js/app.js',
+                    'resources/js/preboarding_table.js',
+                    'resources/js/user.js'],
             refresh: true,
         }),
         vue(),
